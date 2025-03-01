599ad7d4f9060d61795070ba432d080d58bfc90b
Abdelhadikojok <abdelhadikojok8123@gmail.com>
2025-03-01 18:17:51 +0200

    update

--- src/Components/location/Location.js
+++ src/Components/location/Location.js
@@ -47,8 +47,13 @@ export default function Location() {
       <div className="map-container">
         <iframe
           title="Google Map"
-          src="https://maps.google.com/maps?q=Al-Hamidi&t=&z=13&ie=UTF8&iwloc=&output=embed"
-          style={{ width: "100%", height: "400px" }}
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d207.9771068494595!2d35.82727514304606!3d34.44613464656726!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMzTCsDI2JzQ1LjYiTiAzNcKwNDknMzguNiJF!5e0!3m2!1sen!2slb!4v1634567890123!5m2!1sen!2slb"
+          width="100%"
+          height="400"
+          style={{ border: 0 }}
+          allowFullScreen=""
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
         ></iframe>
       </div>
     </div>
